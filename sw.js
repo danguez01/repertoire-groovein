584b9dfdbb2b13545439a28a7dc5a745eff4b6df
danguez01 <61464299+danguez01@users.noreply.github.com>
2026-05-16 20:02:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Le numéro de version est synchronisé avec BUILD_VERSION du HTML.
 // À chaque nouveau build, le cache est invalidé et l'app se rafraîchit.
 
-const VERSION = "20260516-2049";
+const VERSION = "20260516-2100";
 const CACHE_NAME = "groove-in-" + VERSION;
 
 // Ressources à pré-cacher au moment de l'install
